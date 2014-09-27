dd9cc631272c51095bffbd24a575454265bfe6aa
braden <braden@jabblabs.com>
2014-09-27 13:01:15 -0500

    added the chart

--- server/public/javascripts/page.js
+++ server/public/javascripts/page.js
@@ -207,26 +207,83 @@ function loadItem(productId){
 
             // This will get the first returned node in the jQuery collection.
 
-			var radicalData = [result.Rating.Ease,result.FabricFeel,result.Rating.QualityOfFit,result.Rating.Coolness,result.Rating.Design];
-			var radicalGraph = {
+			var radicalData = [result.Rating.Ease,result.Rating.FabricFeel,result.Rating.QualityOfFit,result.Rating.Coolness,result.Rating.Design];
+			console.log(radicalData);
+            var radicalGraph = {
 				labels: ["Ease of Washing", "Fabric Feel", "Quality of Fit", "Coolness", "Design"],
 				datasets: [
 					{
 						label: "Product's ratings",
-						fillColor: "rgba(220,220,220,0.2)",
+						fillColor: "rgba(181, 0, 0, 0.7)",
 						strokeColor: "rgba(220,220,220,1)",
 						pointColor: "rgba(220,220,220,1)",
 						pointStrokeColor: "#fff",
-						pointHighlightFill: "#fff",
+						pointHighlightFill: "#b50000",
 						pointHighlightStroke: "rgba(220,220,220,1)",
 						data: radicalData
 					}
 				]
 			}
             // Get the context of the canvas element we want to select
-            //var ctx = document.getElementById("radicalMenu").getContext("2d");
-            var ctx = $("#radicalMenu").get(0).getContext("2d");
-            var myNewChart = new Chart(ctx).Radar(radicalGraph);
+            var ctx = document.getElementById("radar").getContext("2d");
+            //var ctx = $("#radar").get(0).getContext("2d");
+            var option = {
+                //Boolean - Whether to show lines for each scale point
+                scaleShowLine : true,
+
+                //Boolean - Whether we show the angle lines out of the radar
+                angleShowLineOut : true,
+
+                //Boolean - Whether to show labels on the scale
+                scaleShowLabels : false,
+
+                // Boolean - Whether the scale should begin at zero
+                scaleBeginAtZero : true,
+
+                //String - Colour of the angle line
+                angleLineColor : "rgba(0,0,0,.1)",
+
+                //Number - Pixel width of the angle line
+                angleLineWidth : 1,
+
+                //String - Point label font declaration
+                pointLabelFontFamily : "'Arial'",
+
+                //String - Point label font weight
+                pointLabelFontStyle : "normal",
+
+                //Number - Point label font size in pixels
+                pointLabelFontSize : 10,
+
+                //String - Point label font colour
+                pointLabelFontColor : "#666",
+
+                //Boolean - Whether to show a dot for each point
+                pointDot : true,
+
+                //Number - Radius of each point dot in pixels
+                pointDotRadius : 3,
+
+                //Number - Pixel width of point dot stroke
+                pointDotStrokeWidth : 1,
+
+                //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                pointHitDetectionRadius : 20,
+
+                //Boolean - Whether to show a stroke for datasets
+                datasetStroke : true,
+
+                //Number - Pixel width of dataset stroke
+                datasetStrokeWidth : 2,
+
+                //Boolean - Whether to fill the dataset with a colour
+                datasetFill : true,
+
+                //String - A legend template
+                legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+            };
+            var myNewChart = new Chart(ctx).Radar(radicalGraph, option);
 		}
 	});
 }
